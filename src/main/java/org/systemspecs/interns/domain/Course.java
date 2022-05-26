2d137ab7035df16b9773c1e1c5284197a6c75c45
Ibukun Oduntan <oduntan@systemspecs.com.ng>
2022-05-26 10:50:36 +0100

    created class Course, Programme and Student

---
+++ src/main/java/org/systemspecs/interns/domain/Course.java
@@ -0,0 +1,8 @@
+package org.systemspecs.interns.domain;
+
+public class Course {
+    private Long courseId;
+    private String course_title;
+    private String course_code;
+    private Integer credits;
+}
---
+++ src/main/java/org/systemspecs/interns/domain/Student.java
@@ -0,0 +1,35 @@
+package org.systemspecs.interns.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode//?
+@NoArgsConstructor
+@Entity
+public class Student {
+
+
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+    private String fullName;
+    private String email;
+    private String password;
+    private int level;
+    private String programme;
+
+}
